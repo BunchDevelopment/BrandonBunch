f880a2568b5c7af1977dee3ab1f2eeb1e7f88d13
Brandon Bunch <brandon_bunch@icloud.com>
2019-06-12 18:27:58 -0500

    re push

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 console.log('Hello World!');
 let name = prompt('What is your name?');
-const checkName = function {
-    if(name === ''){
+
+if(name === ''){
     name = prompt('Really? gonna leave me like this? One last time...');
-        if(name === ''){
-            alert('Well Hello anyways stranger.');
-        }
-        else{
-            alert('Hello ' + name);
-        }
+    if(name === ''){
+        alert('Well Hello anyways stranger.');
     }
     else{
         alert('Hello ' + name);
     }
 }
-
-checkName();
+else{
+    alert('Hello ' + name);
+}
