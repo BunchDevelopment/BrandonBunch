b357571e930ed1ad780d96b8318d0024b1a97ac6
Brandon Bunch <brandon_bunch@icloud.com>
2019-06-11 20:46:21 -0500

    created checkName funct

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 console.log('Hello World!');
 let name = prompt('What is your name?');
-
-if(name === ''){
-    name = prompt('Really? gonna leave me like this? One last time...');
+const checkName = function{
     if(name === ''){
-        alert('Well Hello anyways stranger.');
+    name = prompt('Really? gonna leave me like this? One last time...');
+        if(name === ''){
+            alert('Well Hello anyways stranger.');
+        }
+        else{
+            alert('Hello ' + name);
+        }
     }
     else{
         alert('Hello ' + name);
     }
 }
-else{
-    alert('Hello ' + name);
-}
+
+checkName();
