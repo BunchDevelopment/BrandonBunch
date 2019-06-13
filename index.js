c85bfbe179f7211655ab653ca63ac9d801d392b0
Brandon Bunch <brandon_bunch@icloud.com>
2019-06-12 19:17:52 -0500

    troubleshooting index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 console.log('Hello World!');
 let name = prompt('What is your name?');
 
-if(name === ''){
-    name = prompt('Really? gonna leave me like this? One last time...');
+const introPrompt = function() { 
     if(name === ''){
-        alert('Well Hello anyways stranger.');
+        name = prompt('Really? gonna leave me like this? One last time...');
+        if(name === ''){
+            alert('Well Hello anyways stranger.');
+        }
+        else{
+            alert('Hello ' + name);
+        }
     }
     else{
         alert('Hello ' + name);
     }
-}
-else{
-    alert('Hello ' + name);
-}
+};
+
+introPrompt();
