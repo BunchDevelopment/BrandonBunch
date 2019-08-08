d083b8f930f64a2af5a3def93ed7f76ca56258ff
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-07 20:41:57 -0500

    changed gramatical errors

--- components/pages/Blog.js
+++ components/pages/Blog.js
@@ -3,7 +3,7 @@ export default (state) =>`
 <div>
     <div class="container row justify-content-center">
     <h2 class="pageHeader row col-sm-12 animated fadeInLeft">${state.title}</h2>
-    <h1 class="row col-sm-12 justify-content-center">CSS Games</h1>
+    <h1 class="row col-sm-12 justify-content-center">CSS games</h1>
     <h4 class="row col-sm-12 justify-content-center">18 June 2019</h4>
     <p class="animate fadeIn">CSS is an interesting beast. Easy to learn, difficult to master. Developers have created games though, that aim to help you master some of the more difficult aspects of CSS. I played three of them. <a href="https://flexboxfroggy.com/">Flexbox Froggy,</a> <a href="https://cssgridgarden.com/">Grid Garden,</a> and <a href="https://flukeout.github.io/">CSS Diner.</a> With Flexbox Froggy, you use flexbox to move adorable little frogs onto lily pads, which starts out easy, but by the very end, it really tests you knowledge of flexbox. Having never used flexbox before, learning the power of moving items around using flexbox was really interesting. Next was Grid Garden. Ever since I found out what CSS Grid was, I was excited to learn and master grid. Grid Garden has you creating and manipulating grids to water carrots, and poison weeds, all by creating columns, rows, and telling the content were to start and end. Learning how to make content span multiple columns and rows and in general was really exciting. Last we have CSS Diner. This one just has you selecting items that are on a virtual table. This was very helpful. This taught me many ways to use all of the different psudo selectors to help me target elements like a boss. All in all, anyone learning CSS should play these games. They will help make the things you learn, truly stick in memory.</p>
     </div>
@@ -13,7 +13,7 @@ export default (state) =>`
     <!-- Begin Button Section -->
     <section class="coding-help-btns">
     <div class="container row">
-        <h1>Useful Reference Sites for Web Development</h1>
+        <h1>Useful reference sites for web development</h1>
     </div>
     <div class="container row">
     <a href="https://stackoverflow.com/" class="d-flex flex-sm-wrap" target="_blank">
@@ -50,7 +50,7 @@ export default (state) =>`
     </section>
     <hr>
     <div class="container row justify-content-center">
-    <h1 class="row col-sm-12 justify-content-center">Java Script Frameworks</h1>
+    <h1 class="row col-sm-12 justify-content-center">JavaScript frameworks</h1>
     <h4 class="row col-sm-12 justify-content-center">19 June 2019</h4>
     <p><strong>ReactJS. VueJS.</strong> Both frameworks really interest me. React is the most popular JS framework out there right now. React was built by Facebook but is currently operates under the MIT License which makes it open source. So anyone can use it and make changes to it. VueJS is the newest of the two but it is really gathering steam and rising in popularity. It seems to have the same usefulness of React, but there are a few differences between the two. Vue started out as open source and was created by a single developer, Evan You. One of the biggest difference is something called the view layer. With React, you write your code using JSX. With Vue, you use HTML templates, or you can use JSX. but by default you use HTML templates.</p>
     <div class="col-buffer-right"></div>
