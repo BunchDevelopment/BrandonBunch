e6d12276f1d21e593c3246227f2c588a9e97d72b
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-07 20:14:50 -0500

    added content to Goals

--- components/pages/Goals.js
+++ components/pages/Goals.js
@@ -1,3 +1,10 @@
+const reactIMG=require('./../../assets/react_photo.png')
+
 export default (state) =>`
-<h2 class="pageHeader container justify-content-center animated fadeInLeft">${state.title}</h2>
+<div class="container row justify-content-center">
+  <h2 class="pageHeader container justify-content-center animated fadeInLeft">${state.title}</h2>
+  <h1 class="row col-sm-12 justify-content-center">What I Am Doing Next!</h1>
+  <p class="blurIn">Immediately after this portfolio page, I am jumping into learning React, and React Router. The course I am going to take is <a href="https://reactforbeginners.com/" target="_blank">Wes Bos's React for Beginners course</a>. The next project I am going to build is a single page app for a local podcast named "In The Round" and it will be built with React, React Router, and Firebase! All while I hunt for my first development job.</p>
+  <img src="${reactIMG}" class="blur" id="reactIMG"/>
+</div>
 `;
