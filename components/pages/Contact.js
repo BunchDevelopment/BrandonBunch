f6a7e836e7bebf244c584abdb784269101803369
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-07 20:29:43 -0500

    restyled form

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -3,23 +3,22 @@ export default (state) =>`
 <div>
 <h2 class="pageHeader container justify-content-center animated fadeInLeft">${state.title}</h2>     
 </div>
+<div id="centerForm">
 <form action="//formspree.io/brandon_bunch@icloud.com" method="POST">
     <div>
-        <input type="radio" name="subject" value="personal" id="personal" />
-        <label for="personal">Personal message</label>
-        <input type="radio" name="subject" value="professional" id="professional" />
-        <label for="personal">Professional message</label>
-    </div>
-    <div>
+    <label>
         <input type="text" placeholder="First Name" name="firstname" />
-        <br>
+    </label>
+    <label>
         <input type="text" placeholder="Last Name" name="lastname" />
-        <br>
+    </label>
+    <br>
+    <label>
         <input type="email" placeholder="mail@example.com" name="email" />
-        <br>
+    </label>
     </div>
     <div class="messageForm">
-        <label for="marketing">How did you hear about me?</label>
+        <label for="marketing">How did you find my site?</label>
             <select name="marketing">
                 <optgroup label="Online">
                     <option value="social">Social Media (FB, Twitter, LinkedIn)</option>
@@ -34,10 +33,17 @@ export default (state) =>`
                 </optgroup>
             <option value="other">Other</option>
         </select>
+    <div>
+        <input type="radio" name="subject" value="personal" id="personal" />
+        <label for="personal">Personal message</label>
+        <input type="radio" name="subject" value="professional" id="professional" />
+        <label for="personal">Professional message</label>
+    </div>
         <div>
             <textarea name="user_message" rows="8" cols="40" max-length="500" placeholder="Leave me a comment!"></textarea>
         </div> 
     </div>
     <input type="submit" />
 </form>
+</div>
 `;
