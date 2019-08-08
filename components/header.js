87ff4e2d711e736b83ea836e47efde1ee5d5a763
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-07 19:22:18 -0500

    testing netlify fix

--- components/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header(state) { 
-    return `
-    <header class="container">
-        <h2 class="pageHeader col-header animated fadeInLeft">${state.title}</h2>
-    </header>
-`;
-}
